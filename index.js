31f096ba8f4720803cb334cdffe6539331efaa81
¨Mario Castro¨ <mnc634@utexas.edu>
2021-01-12 14:29:02 -0600

    added script code for writing and generating final output file

--- index.js
+++ index.js
@@ -1,16 +1,99 @@
-// array of questions for user
+// Variables with required libraries used in the application
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Variable to generate README markdownn file
+var createREADME = require("./utils/generateMarkdown.js")
+
+// Creates variable with array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "Please type your username?",
+        name: "username"
+    }, 
+    {
+        type: "input",
+        message: "Type your email",
+        name: "email"
+    }, 
+    {
+        type: "input",
+        message: "Type your name/nickname",
+        name: "name"
+    }, 
+    {
+        type: "input",
+        message: "Type a name for your Github repository (ex. https://github.com/reponame",
+        default: "https://github.com/",
+        name: "repoGit"
+    }, 
+    {
+        type: "input",
+        message: "Choose a descriptive title/name for your project",
+        name: "title"
+    }, 
+    {
+        type: "input",
+        message: "Provide a short description of your project",
+        name: "description"
+    }, 
+    {
+        type: "input",
+        message: "Does your project needs a particular npm library or instalation? (Leave blank if not applicable.)",
+        name: "installation",
+        default: "This repository does not require installation."
+    }, 
+    {
+        type: "input",
+        message: "What is this repository for?",
+        name: "use"
+    }, 
+    {
+        type: "list",
+        name: "license",
+        message: "Select a type of license for your project",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    }, 
+    {
+        type: "input",
+        message: "Can other users constribute?",
+        name: "contributors",
+        default: "This repository does not need contributions."
+    }, 
+    {
+        type: "input",
+        message: "Who can users contact for support? (Add email or Github account)",
+        name: "support",
+        default: "This repository does not offer user support"
+    }, 
+    {
+        type: "input",
+        message: "Anyone you want to aknowledge or credit? (Leave blank if you're the only author)",
+        name: "credits",
+        default: "This repository is an original project by the author" 
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, function(err){
+        if(err){
+            return console.log(err);
+            
+        }
+    });
+};
 
 // function to initialize program
 function init() {
-
-}
+    inquirer.prompt(questions)
+    .then(answers =>{
+        writeToFile("./newREADME.md", createREADME(answers));
+    });
+};
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,69 @@
 // Guide for good README file
 
-// 
+// The application will generate a Markdown file with the following Table of Contents: 
+
+// [Author's info] (#About us)
+// [Installation](#installation)
+// [Usage](#Use)
+// [License](#License)
+// [Contributors](#Contributing)
+// [Support](#Support)
+// [Credits](#Credits)
+// [Questions](#questions)
+
+function createMarkdownFile (data) {
+    return;
+
+    `
+    # ${data.title}
+
+    ## Author's info (About Me)
+
+    'This application was created by " + ${data.username}
+    'Contact email: ' + ${data.email}
+    'Github repository: ' + ${data.repoGit}
+
+    ## Installation
+
+    ${data.installation}
+
+    ## Usage
+
+    ${data.use}
+    
+    `;
+
+    `
+    # ${data.title}
+    ## Description
+    ${data.description}
+    ## Table of Contents
+    ${data.tableOfContents}
+    ## Installation
+    ${data.installation}
+    ## Usage
+    ${data.usage}
+    ## License
+    ${data.license}
+    ## Contributing
+    ${data.contributing}
+    ## Tests
+    ${data.tests}
+    ## Questions
+    ${data.questions}
+    `;
+        
+
+}
+
+// username
+// email
+// name
+// repoGit
+// title 
+// description 
+// installation 
+// use 
+// license 
+// support 
+// credits 
